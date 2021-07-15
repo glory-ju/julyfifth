997728db62531164706900f19cc64734dada0290
glory-ju <quzmini@naver.com>
2021-07-15 10:12:26 +0900

    0715/ Web Application 2nd commit

--- julyfifth/settings.py
+++ julyfifth/settings.py
@@ -120,7 +120,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
+# en-us
 
 TIME_ZONE = 'UTC'
 
