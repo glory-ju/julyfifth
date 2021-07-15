54405e79920574dcea01e6c803e737d68afe73ef
glory-ju <quzmini@naver.com>
2021-07-15 09:27:18 +0900

    0715/ Web Application 1st commit

--- julyfifth/settings.py
+++ julyfifth/settings.py
@@ -51,7 +51,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accountapp'
+    'accountapp',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
