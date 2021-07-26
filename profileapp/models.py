7b4923f550e7e017bf379b7ed01ff9f035a1969d
glory-ju <quzmini@naver.com>
2021-07-26 10:15:02 +0900

    0726/ Web Application 3rd commit

--- profileapp/models.py
+++ profileapp/models.py
@@ -9,3 +9,7 @@ class Profile(models.Model):
     image = models.ImageField(upload_to='profile/', null=True)
     nickname = models.CharField(max_length=30, unique=True, null=True)
     message = models.CharField(max_length=200, null=True)
+
+# Terminal 에 python manage.py makemigrations .
+# 에러 뜸? pip install pillow 해주셈
+# python manage.py migrate 해주면 끝
