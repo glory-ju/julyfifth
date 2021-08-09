ecb6daafc37745354a02c8b16b6a8494c8106777
glory-ju <quzmini@naver.com>
2021-08-09 09:41:57 +0900

    0809/ Web Application 2nd commit

--- julyfifth/settings.py
+++ julyfifth/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'bootstrap4',
     'profileapp',
     'articleapp',
+    'commentapp',
 ]
 
 MIDDLEWARE = [
--- julyfifth/urls.py
+++ julyfifth/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('accounts/', include('accountapp.urls')),
     path('profiles/', include('profileapp.urls')),
     path('articles/', include('articleapp.urls')),
+    path('comments/', include('commentapp.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # terminal 에서 python manage.py createsuperuser 입력
