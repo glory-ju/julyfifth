b6cd1cf3d4d3f7215e9a35e6d99137b71441b27b
glory-ju <quzmini@naver.com>
2021-08-10 15:11:35 +0900

    0810/ Web Application 2nd commit migrations

--- commentapp/views.py
+++ commentapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DeleteView
 
 from commentapp.forms import CommentCreationForm
 from commentapp.models import Comment
@@ -21,3 +21,12 @@ class CommentCreateView(CreateView):
 
     def get_success_url(self):
         return reverse('articleapp:detail', kwargs={'pk' : self.object.article.pk})
+
+
+class CommentDeleteView(DeleteView):
+    model = Comment
+    context_object_name = 'target_comment'
+    template_name = 'commentapp/delete.html'
+
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs={'pk':self.object.article.pk})
