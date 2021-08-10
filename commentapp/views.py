b797a25eaf523f6ca47fb7dbdc61f2325f33effc
glory-ju <quzmini@naver.com>
2021-08-10 14:48:16 +0900

    0810/ Web Application 1st commit migrations

--- commentapp/views.py
+++ commentapp/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+from django.views.generic import CreateView
+
+from commentapp.forms import CommentCreationForm
+from commentapp.models import Comment
+
+
+class CommentCreateView(CreateView):
+    model = Comment
+    form_class = CommentCreationForm
+    template_name = 'commentapp/create.html'
+
+    def form_valid(self, form):
+        form.instance.writer = self.request.user
+        form.instance.article_id = self.request.POST.get('article_pk')
+        return super().form_valid(form)
+
+
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs={'pk' : self.object.article.pk})
