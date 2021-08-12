c877b2790fa008a9c6fb119516d72e4ac0e43465
glory-ju <quzmini@naver.com>
2021-08-12 10:41:34 +0900

    0812/ Web Application 4th commit migrations

--- julyfifth/urls.py
+++ julyfifth/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('profiles/', include('profileapp.urls')),
     path('articles/', include('articleapp.urls')),
     path('comments/', include('commentapp.urls')),
+    path('projects/', include('projectapp.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # terminal 에서 python manage.py createsuperuser 입력
